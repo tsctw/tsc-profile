ff8483623e141c26a9f3537b7183360bbf065cca
DawsonChang <tiamat2009@hotmail.com>
2024-02-21 00:05:43 +0100

    profile component buttons

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import { Link } from 'gatsby';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Tooltip } from 'react-tooltip';
+import { RoundButton } from './roundButton';
+
+const copyFunc = () => {
+  const textToCopy = 'taosen.chang@gmail.com';
+
+  const textarea = document.createElement('textarea') as HTMLTextAreaElement;
+  textarea.value = textToCopy;
+  document.body.appendChild(textarea);
+
+  textarea.select();
+  textarea.setSelectionRange(0, 99999); // For mobile devices
+  navigator.clipboard.writeText(textarea.value);
+
+  document.body.removeChild(textarea);
+};
 
 export const Profile = () => {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col justify-center items-center m-3 mt-5">
+      <div className="flex flex-col justify-center items-center m-3 mt-5 profile-image">
         <div className="border-2 rounded-full border-black border-solid relative">
-          <StaticImage className="rounded-full" src="../images/frenchie01.jpeg" alt="profile img" width={200} height={200}></StaticImage>
+          <StaticImage className="rounded-full" src="../images/frenchie01.jpeg" alt="profile img" width={150} height={150}></StaticImage>
         </div>
-        <div className="mt-5">@tsctw</div>
+        <div className="mt-5 font-bold text-xl">Tao-Sen Chang</div>
       </div>
-      {/* <div className="flex gap-4 text-xl">
-          <Link to="https://www.linkedin.com/in/tao-sen-chang/" target="_blank">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </Link>
-          <Link to="https://github.com/tsctw" target="_blank">
-            <FontAwesomeIcon icon={faGithub} />
-          </Link>
-          <a id="email">
-            <Link to="">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </Link>
-          </a>
-          <Tooltip anchorSelect="#email" clickable>
-            <div>taosen.chang@gmail.com</div>
-            <button>Copy</button>
-          </Tooltip>
-        </div> */}
+      <div className="flex justify-center gap-4 text-xl">
+        <RoundButton icon={faLinkedin} to="https://www.linkedin.com/in/tao-sen-chang/" />
+        <RoundButton icon={faGithub} to="https://github.com/tsctw" />
+        <a id="email" onClick={copyFunc}>
+          <RoundButton icon={faEnvelope} to="" />
+        </a>
+        <Tooltip anchorSelect="#email" clickable>
+          <button>taosen.chang@gmail.com</button>
+          {/* <button >Copy</button> */}
+        </Tooltip>
+      </div>
     </div>
   );
 };
